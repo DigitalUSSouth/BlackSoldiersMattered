921c5ac1d3975565020126952322deb0bb6306b2
pathaksid03 <pathak.sid03@gmail.com>
2016-06-13 17:59:19 -0400

    Map 2 working

---
+++ bsm/index2.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'index-2';
+
+require_once ('header.php');
+?>
+    <!-- Content -->
+    <div id="page-container">
+      <h1>This is test map <strong>2</strong></h1>
+      <div id="testmap2"></div>
+      <div id ="timeline"></div>
+      
+    </div>
+<?php 
+require_once ('footer.php');
+?>
