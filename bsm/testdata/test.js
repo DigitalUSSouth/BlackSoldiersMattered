10d4aa5ed29993b9ffb04ec45b4c3e2d2e8efb30
adrq <adrq22@yahoo.com>
2016-06-14 11:41:44 -0400

    add test data

--- bsm/testdata/test.js
+++ bsm/testdata/test.js
@@ -22,7 +22,7 @@ var soldier2 = {
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [
-      [-79.05762, 34.88593],
+      [-78.05762, 34.88593],
       [-98.65723, 30.44867]
     ]
   },
@@ -56,4 +56,181 @@ var soldier3 = {
   }
 };
 
-var soldiers = [soldier1 , soldier2, soldier3];
+var soldier4 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-78.05762, 34.88593],
+      [-94.20801, 41.31082]
+    ]
+  },
+  "properties": {
+    "title" : "soldier1",
+    "path_options" : { "color" : "red" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+var soldier5 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-80.05762, 34.88593],
+      [-92.65723, 30.44867]
+    ]
+  },
+  "properties": {
+    "title" : "soldier2",
+    "path_options" : { "color" : "blue" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+
+var soldier6 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-91.86035, 35.38905],
+      [-100.86426, 38.82259]
+    ]
+  },
+  "properties": {
+    "title" : "soldier3",
+    "path_options" : { "color" : "green" },
+    "time": [
+      500000,
+      505000
+    ]
+  }
+};
+
+var soldier7 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-89.05762, 30.88593],
+      [-93.20801, 42.31082]
+    ]
+  },
+  "properties": {
+    "title" : "soldier1",
+    "path_options" : { "color" : "red" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+var soldier8 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-79.05762, 40.88593],
+      [-98.65723, 35.44867]
+    ]
+  },
+  "properties": {
+    "title" : "soldier2",
+    "path_options" : { "color" : "blue" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+
+var soldier9 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-86.86035, 31.38905],
+      [-105.86426, 32.82259]
+    ]
+  },
+  "properties": {
+    "title" : "soldier3",
+    "path_options" : { "color" : "green" },
+    "time": [
+      500000,
+      505000
+    ]
+  }
+};
+
+var soldier10 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-80.05762, 34.88593],
+      [-93.20801, 30.31082]
+    ]
+  },
+  "properties": {
+    "title" : "soldier1",
+    "path_options" : { "color" : "red" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+var soldier11 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-80.05762, 34.88593],
+      [-108.65723, 30.44867]
+    ]
+  },
+  "properties": {
+    "title" : "soldier2",
+    "path_options" : { "color" : "blue" },
+    "time": [
+      500000,
+      510000
+    ]
+  }
+};
+
+
+var soldier12 = {
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [
+      [-76.86035, 42.38905],
+      [-120.86426, 38.82259]
+    ]
+  },
+  "properties": {
+    "title" : "soldier3",
+    "path_options" : { "color" : "green" },
+    "time": [
+      500000,
+      505000
+    ]
+  }
+};
+
+var soldiers = [soldier1 , soldier2, soldier3,
+                soldier4 , soldier5, soldier6,
+                soldier7 , soldier8, soldier9,
+                soldier10 , soldier11, soldier12];
