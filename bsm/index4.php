7415828cc0599ef157b0a5c83b52f0f3bb1744a6
adrq <adrq22@yahoo.com>
2016-06-15 09:59:07 -0400

    add index4

---
+++ bsm/index4.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'index-4';
+
+require_once ('header.php');
+?>
+    <!-- Content -->
+    <div id="page-container">
+      <h1>This is test map <strong>4</strong></h1>
+      <div id="mapContainer"></div>
+    <div id="tooltip">
+      <svg width="100px" height="0px"></svg>
+
+    </div>
+      
+    </div>
+
+<?php 
+require_once ('footer.php');
+?>
