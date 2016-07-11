42fe937275b2fd4ebd8f0843f2acfefbb16df839
adrq <adrq22@yahoo.com>
2016-07-11 13:01:52 -0400

    update function.php

--- bsm/functions.php
+++ bsm/functions.php
@@ -11,3 +11,84 @@
  function handleException(){
  	//todo this function should handle the any exceptions and gracefully exit the script
  }
+ 
+ 
+ 
+/**
+ * Import tab-delimited file for soldiers random sample and
+ * create soldier.json files
+ */
+function importRandomSample(){
+	ini_set("auto_detect_line_endings", true);
+	$file = NULL;
+	
+	try {
+		$file = new SplFileObject("uploads/random-sample.txt");
+	}
+	catch (Exception $error){
+		echo '<div class="jumbotron"><h1 class="text-danger">Unable to open uploaded file. Please try again.</h1><p>'.$error->getMessage().'</p></div>';
+		return;
+	}
+
+ $counter=0;
+	while($line = $file->fgets()){
+		//discard first 4 lines
+		//TODO: move this outside of loop for performance
+		if ($counter++ < 4) {/*echo $line;*/continue;}
+		
+		//read every other line until the end of file
+		//excel will add double quotes around cells that contain commas
+		//remove double quotes if they are at the edge of a cell
+		//echo $line.'<br>';
+		$line = preg_replace('/\\t"/',"\t", $line);
+		//echo $line.'<br>';
+		$line = preg_replace('/"\\t/',"\t", $line);
+		print $line.'<br>';
+		
+		$fields = explode("\t",$line);
+		
+		$soldier = array(
+				"id"=> $fields[0],
+	"first_name" => $fields[1],
+	"last_name" => $fields[2],
+	"residence_county" => $fields[3],
+	"residence_city" => $fields[4],
+	"entrance_status" => $fields[5],
+	"induction_place" => $fields[6],
+	"induction_date" => $fields[7], /* TODO: convert to ISO 8601 date format */
+	"birth_place" => $fields[],
+	"age" => $fields[], /*we can calculate this from birthdate -Sid 
+			We can calculate age for some but the problem is that
+			we don't have birthdate for every soldiers, but we do
+			have their age - Adrian*/
+	"birth_date" => $fields[], /* ISO 8601 date format */
+	"unit_progression" => [
+			["unit 1"=> "company a", "1917-4-3"], /* does this connect to the unit ID downstairs? -Sid  -- yup!*/
+			["unit 2"=> "company b", "1918-1-3"]
+			/* etc. */ 
+		],
+	"rank_progression" => [
+			"rank 1" => "1917-4-3",
+			"rank 2" => "1918-1-3"
+			/* etc. */ 
+		],
+	"engagements" => [
+			"engagement 1", /* does this mean camps or battles? -Sid --Battles, per the 'engagements' column on the soldiers spreadsheet - Adrian*/
+			"engagement 2"
+			/* etc. */ 
+		],
+	"discharge_date" => $fields[], /* ISO 8601 date format */
+	"service_date_start" => $fields[], /* ISO 8601 date format */
+	"service_date_end" => $fields[], /* ISO 8601 date format */
+	"wounded" => $fields[],
+	"death_date" => $fields[], /* ISO 8601 date format */
+	"death_cause" => $fields[],
+	"death_notified" => $fields[]
+		);
+		
+		
+	}
+	
+	
+	
+}
