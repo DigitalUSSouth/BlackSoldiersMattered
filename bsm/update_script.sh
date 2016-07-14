249283745b2483f3e9e66f545d1e2dbe25d64f81
adrq <adrq22@yahoo.com>
2016-07-14 12:49:06 -0400

    update script

--- bsm/update_script.sh
+++ bsm/update_script.sh
@@ -3,15 +3,15 @@
 
 
 echo "Cloning from github"
+rm -rf bsm-temp
 git clone https://github.com/DigitalUSSouth/BlackSoldiersMattered.git bsm-temp
 
 echo "Creating backup"
-mkdir -p bsm-old
-cd bsm-old
-rm -rf *
-cd ../
-cp -R $(ls | grep -v '^bsm-old$') bsm-old/
-rm -rf !(bsm-temp|bsm-old)
+rm -rf ~/bsm-old/*
+cp -a /var/www/html/bsm ~/bsm-old
+
+shopt -s extglob
+rm -rf !(bsm-temp)
 
 echo "Copying files"
 cp -a bsm-temp/bsm/. /var/www/html/bsm
