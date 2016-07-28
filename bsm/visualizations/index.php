8cabd3ceb07ecff756adc1bd9662cf543881f26d
adrq <adrq22@yahoo.com>
2016-07-28 11:19:47 -0400

    add visualizations index

---
+++ bsm/visualizations/index.php
@@ -0,0 +1,21 @@
+<?php
+$title = 'visualizations';
+
+require_once ('../header.php');
+
+?>
+    <!-- Content -->
+    <div class="row" id="page-container">
+      
+      <div class="jumbotron">
+        <h2>The following pages correspond to slides from the powerpoint. These are in-progress visualizations.</h2>
+        <ul>
+          <li><a href="slide-4">Slide 4 - Types of units pie charts</a></li>
+          <!-- commit -->
+        </ul>
+      </div>
+      
+    </div>
+<?php 
+require_once ('../footer.php');
+?>
