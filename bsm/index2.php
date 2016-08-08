eff55391f8d6586d03c33651b23e82a1deee7c84
adrq <adrq22@yahoo.com>
2016-08-08 00:46:04 -0400

    add possible new homepage index2.php

---
+++ bsm/index2.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="utf-8" />
+  <meta name="author" content="Script Tutorials" />
+  <title>Black Soldiers Mattered</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <!-- attach CSS styles -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/style-v2.css" rel="stylesheet" />
+</head>
+<body>
+  
+  <!-- navigation panel -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Black Soldiers Mattered</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse-main">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#home">Start</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#explore">Explore</a></li>
+        <li><a href="#information">Information</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+  <!-- first section - Home -->
+<div id="home" class="home">
+  <div class="text-vcenter">
+    <h1>Black Soldiers Mattered</h1>
+    <h2>Black North Carolinians as Soldiers in the First World War: A Microcosm of the National African American Experience</h2>
+    <h3>By Janet G. Hudson</h3>
+    <a href="#about" class="btn btn-default btn-lg">Explore</a>
+  </div>
+</div>
+<!-- /first section -->
+  
+  <!-- second section - About -->
+<div id="about" class="pad-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <img src="images/logo.png" alt="" />
+      </div>
+      <div class="col-sm-6 text-center">
+        <h2>subtitle?</h2>
+        <p class="lead">April 6, 1917, changed the lives of 21,609 young African American men from North Carolina The participation of these Black North Carolinians in World War I has been mostly ignored. <strong>Black Soldiers Mattered</strong> addresses that void with this interactive site that allows you to explore the entire journey of Black North Carolinians from induction to demobilization, the specific journey of the many different units they served, and the journey of individual soldiers. </p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /second section -->
+
+  <!-- third section - Services -->
+<div id="explore" class="pad-section">
+  <div class="container">
+    <h2 class="text-center">Explore</h2> <hr />
+    <div class="row text-center">
+      <div class="col-sm-3 col-xs-6">
+        <i class="fa fa-map" aria-hidden="true"></i>
+        <h4>Collective experience</h4>
+        <p>Explore the collective journey of all soldiers and units.</p>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <i class="fa fa-map-marker" aria-hidden="true"></i>
+        <h4>Individual experiences</h4>
+        <p>Choose a Soldier and explore his journey.</p>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        <h4>Timelines</h4>
+        <p>View dynamic timelines that illustrate various aspects of the soldiers' trajectory</p>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <i class="fa fa-pie-chart" aria-hidden="true"></i>
+        <h4>Statistics</h4>
+        <p>Explore charts...</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /third section -->
+
+  <!-- fourth section - Information -->
+<div id="information" class="pad-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Additional information</h2>
+          </div>
+          <div class="panel-body lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit adipiscing blandit. Aliquam placerat, velit a fermentum fermentum, mi felis vehicula justo, a dapibus quam augue non massa. Duis euismod, augue et tempus consequat, lorem mauris porttitor quam, consequat ultricies mauris mi a metus. Phasellus congue, leo sed ultricies tristique, nunc libero tempor ligula, at varius mi nibh in nisi. Aliquam erat volutpat. Maecenas rhoncus, neque facilisis rhoncus tempus, elit ligula varius dui, quis amet. 
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Additional information</h2>
+          </div>
+          <div class="panel-body lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit adipiscing blandit. Aliquam placerat, velit a fermentum fermentum, mi felis vehicula justo, a dapibus quam augue non massa. Duis euismod, augue et tempus consequat, lorem mauris porttitor quam, consequat ultricies mauris mi a metus. Phasellus congue, leo sed ultricies tristique, nunc libero tempor ligula, at varius mi nibh in nisi. Aliquam erat volutpat. Maecenas rhoncus, neque facilisis rhoncus tempus, elit ligula varius dui, quis amet. 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /fourth section -->
+
+  <!-- fifth section -->
+<div id="contact" class="pad-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h3>Contact info would go here?</h3>
+        <h4>contact info</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /fifth section -->
+
+
+<!-- footer -->
+<footer>
+  <hr />
+  <div class="container">
+    <p class="text-right">Copyright &copy; 2016 - University of South Carolina - All Rights Reserved</p>
+  </div>
+</footer>
+<!-- /footer -->
+
+  <!-- attach JavaScripts -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/v2.js"></script>
+</body>
+</html>
---
+++ bsm/js/v2.js
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+
+$(document).ready(function (){
+
+
+  
+});
+
+
+//function to scroll when clicking on navbar
+$("#navbar-collapse-main ul li a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
