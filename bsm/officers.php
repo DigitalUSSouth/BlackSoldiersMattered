fbc12a8d99b10b9e3d5dba48705c6ea39256857b
adrq <adrq22@yahoo.com>
2016-11-04 14:15:26 -0400

    add officers page

---
+++ bsm/officers.php
@@ -0,0 +1,242 @@
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman";
+color:#262626'>In the long list of apprehensions white Americans expressed
+concerning African Americans’ service in World War I, the question of training
+and commissioning black officers proved a major source of political tension. The
+eventual compromise led to one class of officers trained far from the South, in
+Iowa. From the segregated officer training camp at Ft. Des Moines, 1,000 newly
+volunteered civilians and 250 experienced soldiers trained from June to October,
+and 639 African Americans earned their commission on October 15, 1917.<a
+href="#_edn1" name="_ednref1" title=""><span class=MsoEndnoteReference><span
+class=MsoEndnoteReference><span style='font-size:12.0pt;line-height:107%;
+font-family:"Times New Roman";color:#262626'>[i]</span></span></span></a></span></p>
+
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman";
+color:#262626'>Consistent with the national success rates at Ft. Des Moines, at
+least twenty-seven black North Carolinians became officers in October, just
+over half of the forty-nine men identified for officer training. Fourteen North
+Carolinians were commissioned 2<sup>nd</sup> lieutenants, the lowest rank;
+eight as 1<sup>st</sup> lieutenants; and five as captains, the highest rank for
+black soldiers. Sixteen of twenty-seven men commissioned entered Ft. Des Moines
+from civilian life, and the other eleven underwent officer training as
+experienced soldiers. Secretary Baker assigned these officers to infantry,
+artillery, and engineering units of the newly created 92<sup>nd</sup> Division,
+and he assured military and civilian leaders that all commissioned black
+officers would be in the non-southern camps of the segregated division.<a
+href="#_edn2" name="_ednref2" title=""><span class=MsoEndnoteReference><span
+class=MsoEndnoteReference><span style='font-size:12.0pt;line-height:107%;
+font-family:"Times New Roman";color:#262626'>[ii]</span></span></span></a></span></p>
+
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman";
+color:#262626'>These officers were among North Carolina’s most distinguished
+African American soldiers, and many became accomplished men after the war. Commissioned
+officers from civilian life included <b>2<sup>nd</sup> Lt. Robert Smalls
+Bampfield of Wilmington</b>, the grandson of famed Civil War hero and
+Reconstruction era politician from Beaufort, South Carolina, Robert Smalls. Educated
+at Johnson C. Smith, Bampfield also served in World War II, lived much of his
+civilian life in Washington, D.C., and is buried in Arlington National
+Cemetery. Another Johnson C. Smith alumnus <b>2<sup>nd</sup> Lt. Gurney E.
+Nelson of Greensboro,</b> became a career educator after pursuing graduate
+education at the University of Chicago and Ohio State. <b>2<sup>nd</sup> Lt.
+Lovelace B. Capehart, Jr. of Raleigh</b> became a pharmacist and physician
+after the war. <b>2<sup>nd</sup> Lt. Russell C. Atkins of Winston-Salem</b> was
+educated at Hampton Institute in Virginia. Atkins worked before and after the
+war in agricultural education at Tuskegee. His father had been president of
+State Teachers College in Winston Salem. <b>Lt. Bravid W. Harris, Jr. of
+Warrenton</b> became an Episcopal priest and bishop and eventually a missionary
+to Liberia in the ensuing decades. Most black North Carolina officers led men
+of the 365<sup>th</sup> Infantry—the regiment with which most black North
+Carolina infantrymen served. </span><span style='font-size:12.0pt;line-height:
+200%;font-family:"Times New Roman"'>Unfortunately for the state, many of these
+talented and educated native sons who returned from the war seeking
+opportunities found them outside of North Carolina.<a href="#_edn3"
+name="_ednref3" title=""><span class=MsoEndnoteReference><span
+style='color:#262626'><span class=MsoEndnoteReference><span style='font-size:
+12.0pt;line-height:107%;font-family:"Times New Roman";color:#262626'>[iii]</span></span></span></span></a></span></p>
+
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman";
+color:#262626'>Those commissioned officers with prior military experience
+included, among others, <b>captains Moody Staten of Lincoln, Herbert Avery of
+Morganton, and Samuel Reid of Concord and lieutenants James W. Alston of
+Raleigh and Thomas Bullock of Henderson.</b> Except for Capt. Staten, these men
+were in their forties at the time of commissioning. Their ages and military
+experiences set them apart from the other officers and nearly all the enlisted
+men. Lt. Alston, a veteran of the Spanish-American War, had an atypical
+experience for North Carolina black soldiers in other ways. He was one of the
+few North Carolinians who served as an officer in the 93<sup>rd</sup> Division,
+rather than the 92<sup>nd</sup>, leading men in the 372<sup>nd</sup> Infantry
+Regiment. Lt. Alston also corresponded during the war with a white employer,
+Herbert H. Brimley, curator of the State Museum, sharing his experiences and
+perspective. In his correspondence, Lt. Alston continually expressed optimism
+and conveyed his patriotic, anti-German determination to defeat the enemy. “Fritz
+can fight like the very devil when he is under cover and has the most men, but
+can’t stand the <u>Yankee steel</u> and these Yanks, white &amp; black sure
+love to use the bayonet whenever they can get near enough to him.”<a
+href="#_edn4" name="_ednref4" title=""><span class=MsoEndnoteReference><span
+class=MsoEndnoteReference><span style='font-size:12.0pt;line-height:107%;
+font-family:"Times New Roman";color:#262626'>[iv]</span></span></span></a> </span></p>
+
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman";
+color:#262626'>Another of the seasoned soldiers was </span><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman"'>Lt.
+Thomas Bullock, who led men in the 367<sup>th </sup>Infantry. With far more
+military experience than most North Carolinians, Lt. Bullock also had been a
+veteran of the Spanish-American War and had served three years in the
+Philippines with the 25<sup>th</sup> Infantry, one of the U.S. Army’s four
+all-black regular army units. In addition to his military experience, Lt.
+Bullock was college educated. A graduate of Lincoln University, the
+Pennsylvania alma mater of Thurgood Marshall, Lt. Bullock also taught at
+Williston Industrial School in Wilmington. In 1916 when the 15<sup>th</sup>
+Infantry Regiment of the New York National Guard organized, Bullock was among
+the first to volunteer. William Hayward, commanding officer of the New York
+regiment, readily identified him as an ideal candidate for officer training. So
+Bullock left the regiment that later became the 369<sup>th</sup> “Harlem
+Fighters” to lead men in the 92<sup>nd</sup> Division.<a href="#_edn5"
+name="_ednref5" title=""><span class=MsoEndnoteReference><span
+class=MsoEndnoteReference><span style='font-size:12.0pt;line-height:107%;
+font-family:"Times New Roman"'>[v]</span></span></span></a></span></p>
+
+<p class=MsoNormal style='text-indent:.5in;line-height:200%'><span
+style='font-size:12.0pt;line-height:200%;font-family:"Times New Roman"'>While
+Ft. Des Moines only commissioned one class of African American officers, the
+army had a few other opportunities for black soldiers to become commissioned
+officers. African Americans from North Carolina also became commissioned as
+physicians. Similar to most of their national counterparts, medical officers
+from North Carolina, who treated the wounded in combat, were recent medical
+school graduates. Two of those men, <b>Lt. Egbert Theophilus Scott of
+Wilmington and Lt. Max Constuart King of Franklinton, attended Leonard Medical
+College at Shaw University</b>. King finished his studies and graduated from
+Meharry Medical School in Nashville. Although born and educated in North
+Carolina at Biddle University (name changed to Johnson C. Smith in 1923) and
+Shaw University, Scott left his home state after the war for opportunities in
+Pennsylvania where he practiced medicine for fifty years. North Carolinians
+were also among the commissioned black officers in the chaplaincy. For example <b>Lt.
+Alexander Huntington Hatwood, pastor of the Grace A.M.E. Church of Salisbury,
+served as a Methodist chaplain</b> at Camp Taylor in Louisville, Kentucky.<span
+class=MsoEndnoteReference> <a href="#_edn6" name="_ednref6" title=""><span
+class=MsoEndnoteReference><span style='font-size:12.0pt;line-height:107%;
+font-family:"Times New Roman"'>[vi]</span></span></a></span></span></p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+</div>
+
+<div><br clear=all>
+
+<hr align=left size=1 width="33%">
+
+<div id=edn1>
+
+<p class=MsoEndnoteText><a href="#_ednref1" name="_edn1" title=""><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;font-family:"Times New Roman"'><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;line-height:107%;
+font-family:"Times New Roman"'>[i]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> For a thorough
+examination of African American officers see Williams, <i>Torchbearers of
+Democracy</i>, pp 38-51; Barbeau and Henri, <i>Unknown Soldiers</i>, 80-83;
+Emmett J. Scott, <i>Scott’s</i> <i>Official History of the American Negro in
+the World War</i>, (1919, repr., New York: Arno Press, 1969), 82-89. </span></p>
+
+</div>
+
+<div id=edn2>
+
+<p class=MsoEndnoteText><a href="#_ednref2" name="_edn2" title=""><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;font-family:"Times New Roman"'><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;line-height:107%;
+font-family:"Times New Roman"'>[ii]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> Emmett Scott and
+William Sweeney list the 639 commissioned officers. Yet it is difficult to
+identify everyone from these two lists since state identification is only
+available for those who came to officer training as civilians and not for those
+who came from the Regular Army or who joined the National Army before attending
+the school. Sixteen North Carolinians appear in Scott’s and/or Sweeney’s lists.
+I have identified eleven others from my WWIAASC Database. Sarah McCulloh
+Lemmon, <i>North Carolina’s Role in the First World War</i> (Raleigh: North
+Carolina Department of Cultural Resources Division of Archives and History, 1975),
+33. <i>Scott’s Official History</i>, 471-81; William Allison Sweeney, <i>History
+of the American Negro in the Great War</i> (G.G. Sapp, 1919), 86-88. Baker
+ordered the creation of the 92<sup>nd</sup> Division Oct 24, 1917, the week
+following the commissioning of the officer class at Ft. Des Moines. </span></p>
+
+</div>
+
+<div id=edn3>
+
+<p class=MsoNoSpacing style='text-indent:.5in'><a href="#_ednref3" name="_edn3"
+title=""><span class=MsoEndnoteReference><span style='font-size:10.0pt;
+font-family:"Times New Roman"'><span class=MsoEndnoteReference><span
+style='font-size:10.0pt;line-height:107%;font-family:"Times New Roman"'>[iii]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> Andrew Billingsley, <i>Yearning
+to Breathe Free: Robert Smalls of South Carolina and His Families</i>
+(Columbia: University of South Carolina Press, 2010), 197; Omega Psi Phi
+Fraternity website, 2010 </span><a
+href="http://www.tq1911.org/Tau_Omega/Gurney_E._Nelson.html"><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>http://www.tq1911.org/Tau_Omega/Gurney_E._Nelson.html</span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>; Lovelace Capehart
+referenced in Nathan Garrett, <i>A Palette, Not a Portrait: Stories from the
+Life of Nathan Garrett</i> (Bloomington, IN: iUniverse, 2010), 19; Russell C.
+Atkins, Officer Training School in Des Moines, Iowa </span><a
+href="http://www.fortdesmoines.org/graduates_a_g.shtml"><span style='font-size:
+10.0pt;font-family:"Times New Roman"'>http://www.fortdesmoines.org/graduates_a_g.shtml</span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>; Lawrence E. London,
+“Bravid Washington Harris,” in William S. Powell, ed. <i>Dictionary of North
+Carolina Biography</i> (Chapel Hill: University of North Carolina, 1988), vol.
+3, 48-49. </span></p>
+
+</div>
+
+<div id=edn4>
+
+<p class=MsoNoSpacing style='text-indent:.5in'><a href="#_ednref4" name="_edn4"
+title=""><span class=MsoEndnoteReference><span style='font-size:10.0pt;
+font-family:"Times New Roman"'><span class=MsoEndnoteReference><span
+style='font-size:10.0pt;line-height:107%;font-family:"Times New Roman"'>[iv]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> </span><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>NCWWI Service Cards,
+Adjutant General’s Records, NCDAH. Sketch of Alston and James W. Alston to H.H.
+Brimley, November 1, 1918, in James W. Alston Papers Folder, Box 1, VI. Private
+Collections; World War I Papers, 1903-1933; Military Collection; NCDAH.</span><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> </span></p>
+
+</div>
+
+<div id=edn5>
+
+<p class=MsoEndnoteText><a href="#_ednref5" name="_edn5" title=""><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;font-family:"Times New Roman"'><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;line-height:107%;
+font-family:"Times New Roman"'>[v]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> Cleopatra Bullock to
+the Secretary, November 13, 1918; Sketch of Bullock, Roll of Honor folder
+Thomas J. Bullock, Box 5 Negroes and the War, VII. Compiled Individual Service
+Records, World War I Papers, 1903-1933, Military Collection, NCDAH.</span></p>
+
+</div>
+
+<div id=edn6>
+
+<p class=MsoEndnoteText><a href="#_ednref6" name="_edn6" title=""><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;font-family:"Times New Roman"'><span
+class=MsoEndnoteReference><span style='font-size:10.0pt;line-height:107%;
+font-family:"Times New Roman"'>[vi]</span></span></span></span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'> Egbert T. Scott
+Obituary, <i>Afro-American</i>, 16 December 1969, </span><a
+href="http://www.moors-delaware.com/gendat/moors.aspx?Mode=Member&amp;MemberID=S300E1884"><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>http://www.moors-delaware.com/gendat/moors.aspx?Mode=Member&amp;MemberID=S300E1884</span></a><span
+style='font-size:10.0pt;font-family:"Times New Roman"'>; Max King started at
+Leonard. Geraldine Rhoades Beckford, compiler, <i>Biographical Dictionary of
+American Physicians of African Ancestry, 1800-1920</i> (Cherry Hill, NJ:
+Africana Homestead Legacy Publishers Inc., 2011), 188; Arthur Bunyan Caldwell,
+ed., <i>History of the American Negro and His Institutions</i> (A.B. Caldwell
+Publishing, 1921) North Carolina, Vol. 4, 743-44. Foster Burnett of Wilmington
+was another freshly minted physician from Howard who was commissioned a medical
+officer but was discharge within months as “physically disqualified.” NCWWI Service
+Cards, Adjutant General’s Records, NCDAH. Hatwood died in 1947 and was buried
+at the Salisbury National Cemetery. <i>Charlotte City Directory</i>, 1918 list
+his church and the U.S. National Cemetery Interment Control Forms, 1928-1962
+provide his burial information. Both available at Ancestry.com.</span></p>
