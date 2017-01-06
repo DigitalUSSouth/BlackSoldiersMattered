0807cd142746b1f09d7aef70fe26a4503cf86b35
adrq <adrq22@yahoo.com>
2017-01-06 11:24:51 -0500

    update discharge date visualization

--- bsm/bubbles/generateCSV.php
+++ bsm/bubbles/generateCSV.php
@@ -21,10 +21,17 @@ $yearsArray = array();
 
 foreach ($soldierSample as $soldier){
     $date = explode("-",$soldier['discharge_date']);
+    $service = "";
+    if ($soldier['service_date_start']==""){
+        $service = "Domestic";
+    }
+    else {
+        $service = "Overseas";
+    }
     $year = $date[0];
     $month = $date[1];
     $day = $date[2];
-    $newLine = $counter++.",".$soldier['induction_status'].",".$month.",".$day.",".$year."\n";
+    $newLine = $counter++.",".$service.",".$month.",".$day.",".$year."\n";
     //print $newLine.'<br>';
     if (!array_key_exists($year,$yearsArray)){
         $yearsArray[$year] = true;
@@ -35,5 +42,6 @@ foreach ($soldierSample as $soldier){
 //commenting out so we don't override previous file
 //file_put_contents("data/ind5.csv",$output);
 //file_put_contents("data/ind6.csv",$output);
+//file_put_contents("data/ind7.csv",$output);
 
 var_dump($yearsArray);
